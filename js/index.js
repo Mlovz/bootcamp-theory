c97827d9a15a8201853a6ae5b02353bb263a4b25
Yakhya <mestoev.yakhya@mail.ru>
2024-07-29 14:17:03 +0300

    fix

---
+++ js/index.js
@@ -0,0 +1,111 @@
+// Number
+// String
+// Undefined
+
+
+// 
+// = - знак привоение
+// == - знак сравнение по типу данных
+// === - знак сравнение по значению
+
+
+// let bananas = '2'
+// let yellow = bananass
+
+// console.log(yellow)
+
+// console.log(num);
+
+// var num = 10
+// num = 20
+
+
+// let a = 'Nazir';
+// let b = 'Kotiev'
+
+// let result = `${a} ${b}`
+
+// console.log( result);
+
+
+// Преобразование число в строку
+
+// Number
+// String
+// Boolean
+
+// let a = '100'
+// a = 200
+
+
+// let b = Number(a) + 10
+
+// console.log(b);
+
+// let isShow = false
+
+// console.log(typeof isShow);
+
+// Object
+
+// object
+// array
+// null
+
+
+
+// const room = {
+//     height: 100,
+//     width: 200,
+// }
+
+// const user = {
+//     username: 'Test',
+//     firstname: 'Testov',
+//     age: 50,
+// }
+
+// Изменение ключа и значения
+// user.age = 60
+
+// Добавление нового ключа и значения
+// user.lastname = 'Testlast'
+
+// console.log(user);
+
+// skjdfnfn2ugeydubhjas
+// const user = {
+//     username: 'Test',
+//     firstname: 'Testov',
+//     age: 50,
+// }
+
+// skjdfnfn2ugeydubhjas
+// const newUser = {...user}
+// newUser.age = 1000
+// console.log(user);
+
+
+
+const colors = {
+    red: '#000000',
+}
+
+// colors.black = 'asdasd'
+
+// // Доступ к элементу
+// console.log(colors);
+            
+// const arr = ['hi', true, {age: 10}, undefined, null]
+
+// arr.push({age: 10})
+// arr.pop()
+
+// arr.shift()
+// arr.unshift(577)
+
+// console.log(arr.length);
+
+const isAdmin = 2
+
+console.log(isAdmin);
