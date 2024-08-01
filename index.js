778c49df948409d3021a40ee72d18e6993f1eaed
Nazir <nazirkot777@icloud.com>
2024-08-01 14:42:18 +0300

    50leveljsKNA

---
+++ index.js
@@ -0,0 +1,317 @@
+// задание 1
+// const username = 'Nazir'
+// console.log(username);
+
+
+// задание 2
+// let age = 22
+// let city = 'Nazran'
+// console.log(age,city)
+
+
+// задание 3
+// const firstname = 'Nazir'
+// const lastname = 'Kotiev'
+// console.log(`${firstname} ${lastname}`);
+
+
+// задание 4
+// let isStudent = true
+// console.log(isStudent);
+
+
+// задание 5
+// let count = 20
+// count = 25
+// console.log(count);
+
+
+// задание 6
+// let a = 10
+// let b = 20
+// a = 20
+// b = 10
+// console.log(a,b);
+
+
+// задание 7
+// let price = 19.99
+// console.log(typeof price);
+
+
+// задание 8
+// const PI = 3.14
+// console.log(PI);
+
+
+// задание 9
+// let str = '123'
+// str = 123
+// console.log(str);
+
+
+// задание 10
+// let num = 456
+// num = '456'
+// console.log(num);
+
+
+// задание 11
+// let colors = ["red", "green", "blue"]
+// console.log(colors);
+
+
+// задание 12
+// colors.push ('yellow')
+// console.log(colors);
+
+
+// задание 13
+// colors.pop ('yellow')
+// console.log(colors);
+
+
+// задание 14
+// let fruits = ["apple", "banana", "cherry"]
+// let item = fruits [1]
+// console.log(item);
+
+
+// задние 15
+// let person = {}
+// person.username = 'Nazir'
+
+
+// person.age = 22
+//  console.log(person);
+
+
+// задние 16
+// let car = {
+// make: 'bmw',
+// model: 'x5'
+// }
+// console.log(car.make);
+
+
+// задние 17
+// person.age = 23
+// console.log(person.age);
+
+
+// задние 18
+// car.color = 'green'
+// console.log(car);
+
+
+// задние 19
+// delete car.model;
+// console.log(car);
+
+
+// задние 20
+// let firstname = 'Nazir'
+// let age = 22
+// let info = `My name is ${firstname}and I am ${age} years old`
+// console.log(info);
+
+
+// задние 21
+// let isAvailable = false
+// console.log(isAvailable);
+
+
+// задние 22
+// let x = 10
+// let y = 'hello'
+// console.log(typeof x,y);
+
+
+// задние 23
+// let a = 5
+// let b = 10
+// console.log(a==b);
+
+
+
+// задние 24
+// let a = 5
+// let b = 5
+// console.log(a===b);
+
+
+// задние 25
+// let isAdult = 10
+// let hasTicket= 20
+// console.log(isAdult && hasTicket === 10);
+
+
+// задние 26
+// function greet(name) {
+//   console.log(`hello ${name}`);
+// }
+// greet('nazir')
+
+
+// задание 27
+// function square(name) {
+//   console.log(name**name);
+// }
+// square(5)
+
+// задание 28
+// function multiply(a,b=1) {
+//   console.log(a, b);
+// }
+// multiply(10)
+
+// задание 29
+// let num = 10
+// let str = '20'
+// let it = `${num} ${str}`
+// console.log(it);
+
+
+// задание 30
+// let ishappy = true
+// ishappy = String(ishappy)
+// console.log(ishappy);
+
+// задание 32
+
+
+
+// задание 33
+// let text = 'hello'
+// console.log(text.length);
+
+
+// задание 34
+// let studentsdata = [['student',1],['student',2],['student',3]]
+// console.log(studentsdata);
+
+// задание 35
+// let studentsdata = [
+//   ['maga',1],
+//   ['nazir',2],
+//   ['seleyman',3]
+// ]
+// console.log(studentsdata[1][0]);
+
+
+// задание 36
+// const obj = {
+//   greet(){
+//     console.log(`greet`);
+//   },
+//   farewell(){
+//     console.log(`farewell`);
+//   }
+// }
+// obj.greet()
+// obj.farewell()
+
+
+
+// задание 37
+
+// const obj = {
+//   greet(){
+//     console.log(`greet`);
+//   },
+//   farewell(){
+//     console.log(`farewell`);
+//   }
+// }
+// obj.greet()
+
+
+// задание 38
+// const person = {
+//   name: 'Nazir'
+// }
+//  console.log(
+//  person.hasOwnProperty('name')
+//  );
+
+
+// задание 39
+// const person = {
+//   name:'Nazir',
+//   age: 22
+// }
+// const person2 = {...person}
+// console.log(person2);
+
+
+// задание 40
+// const person = {
+//   name: 'Nazir',
+// }
+// const person2 = {
+//   name1: 'suleyman'
+// }
+// const person3 = {...person,...person2}
+// console.log(person3);
+
+
+
+// задание 41
+// let x ;
+
+
+// задание 42
+// console.log(x);
+
+
+
+// задание 43
+// const y = null
+
+
+
+// задание 44
+// const y = null
+// console.log(y);
+
+
+
+// задание 45
+// let numbers = [5,10,'green']
+
+// numbers[0]=3
+// console.log(numbers);
+
+
+// задание 46
+// let numbers = [1,2,3,4,5,6]
+// numbers.forEach(function (num) {
+//   console.log(num);
+// })
+
+
+
+// задание 47
+// let num = 100
+// num = 'name'
+// num = 7
+// console.log(num);
+
+
+// задание 48
+// let str1 = 'apple'
+// let str2 = 'banana'
+// console.log(str1 == str2);
+
+
+// задание 49
+// let numbers = [1,2,3,4,5,]
+// console.log(numbers.length);
+
+
+// задание 50
+// const student = {
+//   address:'nazran',
+//   grades:5
+// }
+// console.log(student);
