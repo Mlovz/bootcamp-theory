7eacd4dbdde141650b0b8b12381b65da497720bd
Suleim <kotievsuleym@gmail.com>
2024-08-01 10:50:55 +0300

    completed ex KS

---
+++ exerciseJs/style.js
@@ -0,0 +1,188 @@
+1.
+// let name = 'Suleim'
+// console.log(name);
+2.
+// let age = 21
+// let city = 'Nazran'
+// console.log(age, city);
+3.
+// let ferstname = 'Suleim' 
+// let lastName = 'Kotiev' 
+// let res = `${ferstname} ${lastName}`
+// console.log(res)
+4.
+// let isStudent = true 
+// console.log(typeof isStudent);
+5.
+// let count = 10 
+// count = 20 
+// console.log(count);
+6.
+// let a = 1
+// let b = 2 
+// a = 2 
+// b = 1
+// console.log(a,b);
+7.
+// let price = 19.99
+// console.log(typeof price);
+8.
+// const PI = 3.14
+// console.log(PI);
+9.
+// let str = '123'
+// console.log(Number (str));
+10.
+// let num = 456 
+// console.log(String(num));
+11.
+// const colors = ["red", "green", "blue"]
+12.
+// colors.push("yellow")
+// console.log(colors);
+13.
+// delete colors[3]
+// console.log(colors);
+14.
+// let fruits = ["apple", "banana", "cherry"]
+// fruits [1]
+// console.log(fruits);
+15.
+// let person = {}
+// person.name = 'Suleim';
+// person.age = 21;
+// console.log(person);
+16.
+// let car = {}
+// car.make = 'Italia'
+// car.model = 'Ferarri'
+// console.log(car.make);
+17.
+// person.age = 221;
+// console.log(person);
+18.
+// car.color = 'red'
+// console.log(car);
+19.
+// delete car.model
+// console.log(car);
+20.
+// let firstname = 'Suleim' 
+// let age = 21 
+// let info = `my name is ${firstname} and i am ${age} years old`
+// console.log(info);
+21.
+// let isAvailable = false
+22.
+// let x = 10
+// let y = 'hello'
+// console.log(typeof y,typeof x);
+23.
+// let a = 5
+// let b = 10 
+// let res = a === b
+// console.log(res);
+24. 
+// let a = 5
+// let b = 5
+// let res = a === b 
+// console.log(res);
+25.
+// let isAdult = true
+// let hasTicket = true
+// const isRes = isAdult && hasTicket === true
+// console.log(isRes);
+26.
+// function greet(name) {
+//   console.log(`hi ${name}` );
+// }
+// greet('Suleim')
+27.
+// function square(params) {
+//   return params ** params
+// }
+// console.log(square(3));
+28.
+// function multiply(params, a=1) {
+//  console.log(params, a);
+// }
+// multiply(10)
+29.
+// let num = 10
+// let str = '20'
+
+// let it = `${num} ${str}`
+// console.log(it);  
+30.
+// let isHappy = true 
+// isHappy = 'String'
+
+// console.log(typeof isHappy);
+31.
+// let colors = ['red', 'black', 'white', 'yellow']
+// let str = colors.join('')
+// console.log(str);
+32.
+// let str = 'Suleim'
+// console.log('Suleim'.split('')); 
+33.
+// let text = 'i am the best'
+// let length = text.length
+// console.log(length);
+34.
+// const num = [[1,2,3,],[4,5,6],[7,8,9] ]
+// console.log(num); 
+35.
+// console.log(num[1][0]);
+36.
+// const greetings = {
+//   greet: 'Hello',
+//   farewall: 'Goodbye',
+//   fullgreet: function() {
+//     return `${this.greet} ${this.farewall}`
+//   }
+// }
+// console.log(greetings.fullgreet());
+37.
+-
+38.
+// console.log('name'in person);
+39.
+// let dublicate = {...person};
+// console.log(dublicate);
+40.
+-
+41.
+// let x 
+42.
+// console.log(typeof x);
+43.
+// let y = null
+44.
+// let res = y === null
+// console.log(res);
+45.
+// let numbers = [1,2,3]
+// const index = 0;
+// const newNum = 10;
+// numbers [index] = newNum;
+// console.log(numbers);
+46.
+// let fruits = ['apple','banana','orange'];
+// fruits.forEach(function (item,i,fruits) {
+//  console.log(i + ": " + item); 
+// })
+47.
+// let num = 100
+// num = '100'
+// num = 10
+// console.log(typeof num);
+48.
+// let str1 = 'apple'
+// let str2 = 'banana'
+// console.log(str1 === str2);
+49.
+// let numbers = [1,2,3,4,5,6,7,8,9]
+// let length = numbers.length
+// console.log(length);
+50. ca
