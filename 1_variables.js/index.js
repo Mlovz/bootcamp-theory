24ccc06e8dee2625026c18de48d2743de638c305
Nazir <nazirkot777@icloud.com>
2024-08-07 16:06:25 +0300

    variablesKN

---
+++ 1_variables.js/index.js
@@ -0,0 +1,357 @@
+// 1 
+// const name = 'Nazir'
+
+
+
+// 2 
+// let age = 22
+// let name = 'Nazir'
+
+
+// 3
+// let firstname = 'Nazir'
+// let lastname = 'Kotiev'
+// console.log(firstname + lastname);
+
+
+
+// 4
+// let isStudent = true
+
+
+// 5
+// let count = 50;
+// count = 100;
+
+
+
+// 6
+// let a = 10;
+// let b = 20;
+// [a, b] = [b, a]
+
+
+
+// 7
+// let price = 19.99;
+// console.log(price);
+
+
+
+// 8
+// const PI = 3.14
+
+
+// 9
+// let str = '123'
+// str = 123
+// console.log(str);
+
+
+
+// 10
+// let num = 456;
+// num = '456'
+// console.log(num);
+
+
+
+// 11
+// let colors = ["red", "green", "blue"]
+// console.log(colors);
+
+
+
+// 12
+// let colors = ["red", "green", "blue"]
+// colors.push ("yellow") 
+// console.log(colors);
+
+
+
+// 13
+// let colors = ["red", "green", "blue"]
+// colors.pop () 
+// console.log(colors);
+
+
+// 14
+// let fruits = ["apple", "banana", "cherry"]
+// console.log(fruits [1]);
+
+
+// 15
+// let person = {
+//   name1: 'Nazir',
+//   age2: 22,
+// }
+// console.log(person);
+
+
+
+// 16
+// let car = {
+// make: "bmw",
+// model: "f90"
+// }
+//  console.log(car.make);
+ 
+
+// 17
+// let person = {
+//   age2: 23
+// }
+// console.log(person);
+
+
+
+// 18
+// let car = {
+//   color: 'red'
+// }
+// console.log(car);
+
+
+
+// 19
+// delete car.model
+
+
+
+
+// 20
+// let firstName = 'Nazir'
+// let age = 22
+// console.log(`My name is $[firstName] and I am $[age] years old.`);
+
+
+
+// 21
+// let isAvailable = false;
+
+
+
+// 22
+// let x = 10
+// let y = 'Hello'
+// console.log(typeof x);
+// console.log(typeof y);
+
+
+
+// 23
+// let a = 5
+// let b = 10
+// console.log(a == b);
+
+
+
+// 24
+// let a = '5'
+// let b = 5
+// console.log(a === b);
+
+
+// 25
+// let isAdult = true
+// let hasTicket = true
+// console.log(typeof isAdult && hasTicket);
+
+
+
+// 26
+// function greet(name) {
+//   console.log(`hello ${name}`);
+// }
+// greet("Nazir")
+
+
+// 27
+// function square(number) {
+//   return number * number;
+// }
+// console.log(square(5));
+
+
+// 28
+// function multiply(a, b = 1) {
+//   return a * b;
+// }
+
+
+
+
+// 29
+// let num = 10
+// let str = '20'
+// console.log(num + str);
+
+
+
+// 30
+// let isHappy = true
+// isHappy = string'isHappy'
+// console.log(isHappy);
+
+
+
+
+// 31
+// let num = [1,2,3]
+// let arr = num.join('')
+// console.log(arr);
+
+
+
+// 32
+// let num = "masive"
+// arr = num.split("");
+
+
+
+// 33
+// let text = 'magec'
+// console.log(text.length);
+
+
+
+// 34
+// let masive = [
+//   [1,2,3],
+//   [4,5,6],
+// ];
+// console.log(masive[1][2]);
+
+
+
+// 35
+// let masive = [
+//   [1,2,3],
+//   [4,5,6],
+// ];
+// console.log(masive[1][0]);
+
+
+
+// 36
+// let obj = {
+// name: 'Nazir',
+// lastname: 'Kotiev',
+// greet: function () {
+//   console.log(`hello my name is ${this.name} `);
+//   }, 
+// farewell: function () {
+//   console.log(`hello my lastname is ${this.lastname}`);
+// },
+// };
+// console.log(obj);
+
+
+
+// 37
+// let obj = {
+// name: 'Nazir',
+// lastname: 'Kotiev',
+// greet: function () {
+//   console.log(`hello my name is ${this.name} `);
+//   }, 
+// farewell: function () {
+//   console.log(`hello my lastname is ${this.lastname}`);
+// },
+// };
+// obj.greet()
+
+
+// 38
+// let person = {
+//   name: 'Nazir',
+
+// }
+// console.log('name' in person);
+
+
+
+// 39
+// let person = {
+//   name: 'Nazir',
+// }
+// let person2 = {...person}
+// console.log(person2);
+
+
+
+// 40
+// let person1 = {
+//   name: 'Nazir'
+// }
+// let person2 = {
+//   age: 22
+// }
+// let person3 = {...person1,...person2}
+// console.log(person3);
+
+
+
+// 41
+// let x 
+
+
+
+// 42
+// console.log(typeof x)
+
+
+
+// 43
+// let y = null
+
+
+
+// 44
+// console.log(typeof null);
+
+
+
+// 45
+// let numbers = [1,2,3,4,5];
+// numbers[3] = 2;
+// console.log(numbers);
+
+
+
+// 46
+// let numbers = [1,2,3,4,5];
+// numbers.forEach((element) => console.log(element))
+
+
+
+// 47
+// let num = 100;
+// num = String (num)
+// num = Number (num)
+// console.log(num);
+
+
+// 48
+// let str1 = "apple"
+// let str2 = "banana"
+// console.log(str1 == str2);
+
+
+
+// 49
+// let numbers = [1,2,3,4,5,6,7];
+// console.log(numbers.length);
+
+
+
+// 50
+let student = {
+ address: {
+  city: 'moskov',
+},
+ grades: {
+  number: 20,
+},
+}
+console.log(student);
