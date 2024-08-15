fac94a6e11a5de2e964bf78282c92b51598f3989
Suleim <kotievsuleym@gmail.com>
2024-08-15 14:01:09 +0300

    fixGameKS

--- games/main.js
+++ games/main.js
@@ -2,14 +2,14 @@ const input = document.querySelector('input')
 const button = document.querySelector('button')
 const randomNumber = Math.floor(Math.random() * 100)
 const result = document.querySelector('.result')
-let attempts = 0
+let attempts = 3
 
 button.addEventListener('click', () => {
-attempts++;
-if (input.value == randomNumber) {
+attempts--;
+if (Number(input.value) === randomNumber) {
   button.setAttribute('disabled', '');
   result.style.color = 'green';
-  result.textContent = `Вы выиграли с ${attempts} попытки`; 
+  result.textContent = `Вы выиграли с ${attempts -1} попытки`; 
   
 } else if (input.value < randomNumber) {
   result.style.color = 'red'
@@ -17,6 +17,12 @@ if (input.value == randomNumber) {
 } else {
   result.style.color = 'red'
   result.textContent = 'Вы ввели слишком большое число'
+} if (attempts <= 0) {
+  result.style.color = 'red'
+  result.textContent = 'Попытки закончились'
+  button.setAttribute('disabled', '');
+} {
+
 }
 input.value = '';
 })
