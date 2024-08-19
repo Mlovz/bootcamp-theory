a08062c85d03d828a7bbb2dc65ee7486a45fa38a
Timur Gorbakov <gerodot050695@gmail.com>
2024-08-19 14:22:53 +0300

    fix

--- games/game3/main.js
+++ games/game3/main.js
@@ -4,18 +4,21 @@ let computerChoice;
 
 function playGame() {
   computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  playerChoice = document.getElementById('playerChoice').value;
+  playerChoice = document.getElementById('playerChoice');
   document.getElementById('result').innerHTML = '';
 
-  if (playerChoice === computerChoice) {
+  let inputValue = playerChoice.value 
+
+  if (inputValue === computerChoice) {
     document.getElementById('result').innerHTML = 'Ничья';
   } else if (
-    (playerChoice === 'камень' && computerChoice === 'ножницы') ||
-    (playerChoice === 'бумага' && computerChoice === 'камень') ||
-    (playerChoice === 'ножницы' && computerChoice === 'бумага')
+    (inputValue === 'камень' && computerChoice === 'ножницы') ||
+    (inputValue === 'бумага' && computerChoice === 'камень') ||
+    (inputValue === 'ножницы' && computerChoice === 'бумага')
   ) {
     document.getElementById('result').innerHTML = `Победа`;
   } else {
     document.getElementById('result').innerHTML = `Поражение`;
   }
+  playerChoice.value = ""
 }
